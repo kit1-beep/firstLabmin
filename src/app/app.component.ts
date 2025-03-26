da34abef8600aa1a3a2ee31a2da911f06f04170c
kyle kitchener <kyle@labmin.com>
2025-03-26 08:46:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PanelMenuModule,ButtonModule ],
+  
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <router-outlet></router-outlet>
     <router-outlet />
+    <p-button label="Toggle All" [text]="true" (onClick)="toggleAll()" />
+<p-panelmenu [model]="items" styleClass="w-full md:w-80" />
   `,
   styles: [],
 })
 export class AppComponent {
+  items = [];
+  toggleAll(){
+    // items = [name];
+
+  }
   title = 'firstLabmin';
 }
